2f48d374e9463ded8f16fe983736d77571c4f0dd
hitohira <hitohirasakuno@gmail.com>
2019-11-28 16:44:10 +0900

    start locating

---
+++ locate.c
@@ -0,0 +1,68 @@
+#include "mpi_util.h"
+
+// return value is the size of res
+static int factorize(int n,int** res){
+	int sz = 16;
+	*res = (int*)malloc(sz*sizeof(int));
+
+	for(int t = 0;;t++){
+		if(n <= 1){
+			return t;
+		}
+
+		for(int i = 2; i <= n;i++){
+			if(n % i == 0){
+				(*res)[t] = i;
+				n /= i;
+				break;
+			}
+		}
+
+		if(t == sz - 1){
+			sz *= 2;
+			int* tmp = (int*)realloc(*res,sz);
+			*res = tmp;
+		}
+	}
+}
+
+void get_dims(int n,int ndims,const int global[],int local[]){
+	int* primes = NULL;
+	int len = factorize(n,&primes);
+
+	// TODO
+}
+
+int cart_create(MPI_Comm comm_old,int ndims,const int dims[],MPI_Comm* comm_cart){
+	int end_stat = 0;
+	int gSize,gId,lSize,lId;
+	MPI_Comm splited;
+	int* info = NULL;
+	int* local = NULL;
+
+	nodeSplit(comm_old,&splited);
+	end_stat = gatherSplitInfoTo0(comm_old,splited,&info);
+	if(end_stat == -1){
+		goto fine;
+	}
+	
+	MPI_Comm_size(comm_old,&gSize);
+	MPI_Comm_rank(comm_old,&gId);
+	MPI_Comm_size(splited,&lSize);
+	MPI_Comm_rank(splited.&lId);
+
+	local = (int*)malloc(ndims*sizeof(int));
+	if(local == NULL){
+		end_stat = -1;
+		goto fine;
+	}
+
+	get_dims(lSize,ndims,dims,local);
+
+	// TODO reorder
+
+fine:
+	if(info) free(info);
+	if(local) free(local);
+	return end_stat;
+}
